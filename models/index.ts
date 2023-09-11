8e755e35d9d95f66d5cca4a71d1db62b34177b35
Ricardo <ricardocavalcantecs@gmail.com>
2023-09-11 08:20:33 -0300

    obtendo 3 cursos em destaque

--- models/index.ts
+++ models/index.ts
@@ -7,7 +7,7 @@ import { User } from './user'
 Category.hasMany(Course,{as: 'courses'})
 
 Course.belongsTo(Category)
-Course.hasMany(Episode)
+Course.hasMany(Episode , {as:'episodes'})
 
 Episode.belongsTo(Course)
 
